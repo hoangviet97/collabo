6a0bee42e7dacc6df5b59628b3589bdba34aaf92
Hoang Pham <vitekpham97@seznam.cz>
2021-02-11 21:14:16 +0100

    Create database connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const connection = require("./config/db");
 const app = express();
 
 require("dotenv").config();
@@ -10,6 +11,11 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("DB connected!");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}...`);
