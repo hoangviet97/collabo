5d2618645e2895b4dac149a770746865f78bdaf5
Hoang Pham <vitekpham97@seznam.cz>
2021-02-26 22:47:38 +0100

    Added register component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
 const App = () => {
   return (
     <div className="app">
-      <Login />
+      <Register />
     </div>
   );
 };
---
+++ frontend/src/components/auth/Register.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { Form, Input, Button } from "antd";
+
+const Register = () => {
+  return (
+    <div className="base-wrapper">
+      <div class="auth-header">
+        <h1 className="auth-logo">collabo</h1>
+      </div>
+      <Form className="form-box">
+        <div class="form-auth-header">
+          <h2>Sign Up</h2>
+        </div>
+        <Form.Item
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: "Please input your e-mail"
+            }
+          ]}
+        >
+          <Input placeholder="E-mail" />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!"
+            }
+          ]}
+        >
+          <Input.Password placeholder="Password" />
+        </Form.Item>
+        <Form.Item
+          name="Re-type password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!"
+            }
+          ]}
+        >
+          <Input.Password placeholder="Re-type Password" />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Submit
+          </Button>
+        </Form.Item>
+        <div className="form-auth-footer">
+          <p>
+            Already have an account? <a> Login</a>
+          </p>
+        </div>
+      </Form>
+    </div>
+  );
+};
+
+export default Register;
