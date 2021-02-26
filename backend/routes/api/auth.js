6f7575bbb1705266b321646dcb1da10ad9b1a00f
Hoang Pham <vitekpham97@seznam.cz>
2021-02-26 22:41:09 +0100

    refactor project structure

--- backend/routes/api/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const authController = require("../../controllers/user.controller");
-const router = express.Router();
-
-router.post("/register", authController.create);
-router.post("/login", authController.login);
-
-module.exports = router;
--- backend/routes/api/projects.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const projectController = require("../../controllers/project.controller");
-const auth = require("../../middleware/auth");
-const router = express.Router();
-
-router.post("/add", auth, projectController.create);
-router.get("/", auth, projectController.getAll);
-
-module.exports = router;
