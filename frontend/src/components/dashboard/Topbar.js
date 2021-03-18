6bce09ba97acbc48f92e006bc1026ba6946df72d
Hoang Pham <vitekpham97@seznam.cz>
2021-03-18 20:31:47 +0100

    Changed avatar position to topbar

--- frontend/src/components/dashboard/Topbar.js
+++ frontend/src/components/dashboard/Topbar.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Avatar } from "antd";
+import { UserOutlined } from "@ant-design/icons";
 
 const Topbar = () => {
-  return <div className="topbar">topbar</div>;
+  return (
+    <div className="topbar">
+      <div class="topbar-profile">
+        <Avatar size="large" icon={<UserOutlined />} />
+        <span>Alexander</span>
+      </div>
+    </div>
+  );
 };
 
 export default Topbar;
