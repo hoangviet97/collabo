6b952e3902440f0fd179765d60dd91278db1d5c0
Hoang Pham <vitekpham97@seznam.cz>
2021-03-23 06:04:23 +0100

    Added profile api and route

--- backend/routes/api/auth.js
+++ backend/routes/api/auth.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const authController = require("../../controllers/user.controller");
+const userController = require("../../controllers/user.controller");
+const authController = require("../../controllers/auth.controller");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
 router.post("/register", authController.create);
 router.post("/login", authController.login);
+router.get("/profile", auth, userController.getCurrentUser);
 
 module.exports = router;
