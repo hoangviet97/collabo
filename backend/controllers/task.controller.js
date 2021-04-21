852cbd496d480f6101d9c69782d717030dab12a8
Hoang Pham <vitekpham97@seznam.cz>
2021-04-21 23:34:46 +0200

    Added members_tasks model and controller connection to new task

--- backend/controllers/task.controller.js
+++ backend/controllers/task.controller.js
@@ -6,7 +6,15 @@ module.exports = {
   create: function (req, res) {
     Task.createTask(req.body, (err, result) => {
       if (err) return apiResponse.ErrorResponse(res, err.message);
-      return res.json(result);
+
+      if (result !== null) {
+        Task.createTaskAssignees(result, (err, resu) => {
+          if (err) return apiResponse.ErrorResponse(res, err.message);
+          return res.json(resu);
+        });
+      } else {
+        return res.json(result);
+      }
     });
   }
 };
--- backend/model/Task.js
+++ backend/model/Task.js
@@ -13,11 +13,34 @@ module.exports = {
       description: body.description,
       start_date: body.start_date,
       due_date: body.due_date,
-      created_at: new Date()
+      created_at: new Date(),
+      assignees: body.assignees
     };
 
     const sql = `INSERT INTO tasks (id, sections_id, priorities_id, task_status_id, name, description, start_date, due_date, created_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
     con.query(sql, [newTask.id, newTask.sectionId, newTask.priorityId, newTask.statusId, newTask.name, newTask.description, newTask.start_date, newTask.due_date, newTask.created_at], (err, res) => {
+      if (err) {
+        result(err, null);
+        return;
+      }
+
+      if (newTask.assignees) {
+        result(null, { assignees: newTask.assignees, task: newTask.id });
+        return;
+      } else {
+        result(null, null);
+        return;
+      }
+    });
+  },
+
+  createTaskAssignees: async function (body, result) {
+    const arr = [];
+    const { assignees, task } = body;
+    assignees.map((assignee) => arr.push([assignee, task]));
+
+    const sql = `INSERT INTO members_tasks (members_id, tasks_id) VALUES ?`;
+    con.query(sql, [arr], (err, res) => {
       if (err) {
         result(err, null);
         return;
