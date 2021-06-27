30916458216aaaafb1822b1946159be7a0d83f60
Hoang Pham <vitekpham97@seznam.cz>
2021-06-27 15:06:22 +0200

    Added channel routing

--- frontend/src/components/dashboard/currentProject/chat/ChannelList.js
+++ frontend/src/components/dashboard/currentProject/chat/ChannelList.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { Link, useRouteMatch } from "react-router-dom";
 import { connect } from "react-redux";
 import { createChannel, getAllChannels } from "../../../../actions/channel";
 
@@ -7,12 +8,14 @@ const ChannelList = (props) => {
     props.getAllChannels({ id: props.projectId });
   }, []);
 
+  let { path, url } = useRouteMatch();
+
   return (
     <div className="channel">
       <ul className="channel__list" style={{ listStyleType: "none" }}>
         {props.channels.map((channel) => (
           <li className="channel__item" key={channel.id}>
-            # &nbsp; {channel.name}
+            <Link to={`${url}/${channel.id}`}># &nbsp; {channel.name}</Link>
           </li>
         ))}
       </ul>
--- frontend/src/components/dashboard/currentProject/chat/ChatContent.js
+++ frontend/src/components/dashboard/currentProject/chat/ChatContent.js
@@ -1,11 +1,8 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 
 const ChatContent = () => {
-  return (
-    <div className="meeting__content" style={{ overflowY: "scroll", backgroundColor: "white", padding: "15px", height: "calc(100vh - 120px)", flex: 1, borderRadius: "12px" }}>
-      <span>Available Sessions</span>
-    </div>
-  );
+  return <div className="meeting__content" style={{ overflowY: "scroll", backgroundColor: "white", padding: "15px", height: "calc(100vh - 120px)", flex: 1, borderRadius: "12px" }}></div>;
 };
 
 export default ChatContent;
