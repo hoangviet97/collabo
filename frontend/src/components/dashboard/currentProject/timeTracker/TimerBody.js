4e95c7fdf58e88c01e16d01cb7a021eed785d476
Hoang Pham <vitekpham97@seznam.cz>
2021-08-26 12:00:51 +0200

    Added today time in header

--- frontend/src/components/dashboard/currentProject/timeTracker/TimerBody.js
+++ frontend/src/components/dashboard/currentProject/timeTracker/TimerBody.js
@@ -1,10 +1,9 @@
 import React from "react";
-import Timer from "./Timer";
 
 const TimerBody = () => {
   return (
     <div className="time-body" style={{ width: "100%", height: "50vh", marginTop: "20px", display: "flex", gap: "20px" }}>
-      <div className="time-body__chart" style={{ backgroundColor: "white", width: "50%", height: "inherit", borderRadius: "12px" }}></div>
+      <div className="time-body__chart" style={{ padding: "0 20px", backgroundColor: "white", width: "50%", height: "inherit", borderRadius: "12px", display: "flex", alignItems: "center" }}></div>
       <div className="time-body__chart" style={{ backgroundColor: "white", width: "50%", height: "inherit", borderRadius: "12px" }}></div>
     </div>
   );
--- frontend/src/components/dashboard/currentProject/timeTracker/TimerHeader.js
+++ frontend/src/components/dashboard/currentProject/timeTracker/TimerHeader.js
@@ -1,45 +1,51 @@
 import React, { useEffect, useState } from "react";
-import moment from "moment";
 import { connect } from "react-redux";
+import moment from "moment";
 import { getTimeRecords } from "../../../../actions/time_record";
 
 const TimerHeader = (props) => {
   const [total, setTotal] = useState(0);
-  const [totalHours, setTotalHours] = useState("");
-  const [totalminutes, setTotalMinutes] = useState("");
+  const [totalToday, setTotalToday] = useState(0);
 
   useEffect(() => {
+    const today = new Date();
     props.data.map((item) => {
       setTotal((prev) => prev + item.total);
     });
 
+    props.data.filter((i) => moment(i.created_at).format("YYYY MM DD") === moment(today).format("YYYY MM DD")).map((item) => setTotalToday((prev) => prev + item.total));
+
     return () => {
       setTotal(0);
+      setTotalToday(0);
     };
   }, [props.data]);
 
   return (
-    <div className="time-header" style={{ backgroundColor: "white", width: "100%", height: "120px", borderRadius: "12px", display: "flex", justifyContent: "space-between", padding: "20px" }}>
-      <div class="time-header__item" style={{ width: "25%", borderRight: "0.8px solid #ecf0f1" }}>
-        <div className="time-header__container" style={{ display: "flex", flexDirection: "column", padding: "0 15px" }}>
+    <div className="time-header">
+      <div class="time-header__item">
+        <div className="time-header__container">
           <span>Total Time</span>
-          <span style={{ wordSpacing: "2px", fontSize: "32px", fontWeight: "bolder" }}>
+          <span className="time-header__value">
             {`${Math.floor(total / 3600)}`.slice(-2)}h {`0${Math.floor(total / 60) % 60}`.slice(-2)}m
           </span>
         </div>
       </div>
-      <div class="time-header__item" style={{ width: "25%", borderRight: "0.8px solid #ecf0f1" }}>
-        <div className="time-header__container" style={{ display: "flex", flexDirection: "column", padding: "0 15px" }}>
+      <div class="time-header__item">
+        <div className="time-header__container">
           <span>Time Today</span>
+          <span className="time-header__value">
+            {`${Math.floor(totalToday / 3600)}`.slice(-2)}h {`0${Math.floor(totalToday / 60) % 60}`.slice(-2)}m
+          </span>
         </div>
       </div>
-      <div class="time-header__item" style={{ width: "25%", borderRight: "0.8px solid #ecf0f1" }}>
-        <div className="time-header__container" style={{ display: "flex", flexDirection: "column", padding: "0 15px" }}>
-          <span>Total Time</span>
+      <div class="time-header__item">
+        <div className="time-header__container">
+          <span>Daily Avg time</span>
         </div>
       </div>
       <div class="time-header__item" style={{ width: "25%" }}>
-        <div className="time-header__container" style={{ display: "flex", flexDirection: "column", padding: "0 15px" }}>
+        <div className="time-header__container">
           <span>Total Time</span>
         </div>
       </div>
