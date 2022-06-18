074c39d622deefbb97cff47b0a3f5ad752b93793
Hoang Pham <vitekpham97@seznam.cz>
2022-06-18 14:01:38 +0200

    Refactored tag model/controllers

--- backend/model/Tag.js
+++ backend/model/Tag.js
@@ -13,32 +13,21 @@ class Tag {
 module.exports = {
   Tag,
   // create new member by user or by admin
-  create: async function (body, result) {
-    const newTag = new Tag(uuid4(), body.project, body.name, body.color);
-
+  create: async function (body, project) {
+    const newTag = new Tag(uuid4(), project, body.name, body.color);
     const sql = `INSERT INTO tags (id, projects_id, name, color) VALUES (?, ?, ?, ?)`;
-    con.query(sql, [newTag.id, newTag.projects_id, newTag.name, newTag.color], (err, res) => {
-      if (err) {
-        result(err, null);
-        return;
-      }
 
-      result(null, newTag);
-      return;
-    });
+    const result = await con.promise().query(sql, [newTag.id, newTag.projects_id, newTag.name, newTag.color]);
+
+    return newTag;
   },
 
-  createTaskTag: async function (body, result) {
+  createTaskTag: async function (body) {
     const sql = `INSERT INTO tasks_has_tags (tasks_id, tags_id) VALUES (?, ?)`;
-    con.query(sql, [body.task, body.tag], (err, res) => {
-      if (err) {
-        result(err, null);
-        return;
-      }
 
-      result(null, "succeess");
-      return;
-    });
+    const result = await con.promise().query(sql, [body.task, body.tag]);
+
+    return result;
   },
 
   // find all tags
@@ -50,7 +39,7 @@ module.exports = {
     return rows;
   },
 
-  findByTasks: async function (project, result) {
+  findByTasks: async function (project) {
     const sql = `SELECT tags_id, tasks_id, tags.name, tags.color
                     FROM tasks_has_tags 
                     INNER JOIN tags ON tasks_has_tags.tags_id = tags.id
@@ -59,28 +48,16 @@ module.exports = {
                     INNER JOIN projects ON sections.projects_id = projects.id
                     WHERE projects.id = ?`;
 
-    con.query(sql, [project], (err, res) => {
-      if (err) {
-        result(err, null);
-        return;
-      }
+    const [rows] = await con.promise().query(sql, [project]);
 
-      result(null, res);
-      return;
-    });
+    return rows;
   },
 
-  deleteTag: async function (id, result) {
+  deleteTag: async function (id) {
     const sql = `DELETE FROM tags WHERE id = ?`;
 
-    con.query(sql, [id], (err, res) => {
-      if (err) {
-        result(err, null);
-        return;
-      }
+    const [rows] = await con.promise().query(sql, id);
 
-      result(null, res);
-      return;
-    });
+    return rows;
   }
 };
